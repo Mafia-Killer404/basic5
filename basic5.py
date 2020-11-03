190497a032fc524f66ae23edd900d5c898c0765a
Mafia-Killer404 <72305102+Mafia-Killer404@users.noreply.github.com>
2020-11-03 19:24:13 +0500

    Add files via upload

---
+++ basic5.py
@@ -0,0 +1,148 @@
+ #!/usr/bin/python3
+import os
+import time
+import sys
+ 
+os.system("clear")
+ 
+print('''\033[1;91m●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
+\033[1;92m●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
+
+\033[1;93m●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
+\033[1;94m●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
+
+*_ꙮ⃢▄▆▇█�⃢✿⁍⃢🇪⃢⁌✿⃢��⃢✿⁍⃢🇪⃢⁌✿⃢��⃢✿⁍⃢🇪⃢⁌✿⃢�⃢✿⁍⃢🇪⃢⁌✿⃢��█▇▆▄⃢ꙮ_*
+
+\033[1;91m        █▀▄▀█ █▀▀█ █▀▀ ░▀░ █▀▀█
+\033[1;93m        █░▀░█ █▄▄█ █▀▀ ▀█▀ █▄▄█
+\033[1;92m        ▀░░░▀ ▀░░▀ ▀░░ ▀▀▀ ▀░░▀
+
+\033[1;97m        █░█ ░▀░ █░░ █░░ █▀▀ █▀▀█
+\033[1;96m        █▀▄ ▀█▀ █░░ █░░ █▀▀ █▄▄▀
+\033[1;95m        ▀░▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀░▀▀
+
+*_ꙮ⃢▄▆▇█�⃢✿⁍⃢🇪⃢⁌✿⃢��⃢✿⁍⃢🇪⃢⁌✿⃢��⃢✿⁍⃢🇪⃢⁌✿⃢�⃢✿⁍⃢🇪⃢⁌✿⃢��█▇▆▄⃢ꙮ_*
+
+\033[1;91m●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
+\033[1;92m●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
+
+\033[1;93m●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
+\033[1;94m●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+ 
+print (''' \033[92m
++--------------------------------------+
+| This Tool Install All Basic Packages
+|         in  ONE COMMAND  
++--------------------------------------+
+| Coded By 💕FAROOQ ANSARI💕 | version : 2.0  |
++--------------------------------------+''')
+ 
+slowprint(''' \033[96m
+[01] python
+[02] python2
+[03] python-dev
+[04] python3
+[05] php
+[06] java
+[07] git
+[08] perl
+[09] bash
+[10] nano
+[11] curl
+[12] openssl
+[13] openssh
+[14] wget
+[15] clang
+[16] nmap
+[17] w3m
+[18] hydra
+[19] ruby
+[20] macchanger
+[21] host
+[22] dnsutils
+[23] requests
+[24] mechanize
+[25] lolcat
+[26] figlet
+[27] wget
+[28] termux-setup-storage
+[29] chmod +x *
+[30] toilet
+[31] unzip
+[32] termcolor
+[33] sudo
+[34] ngrok
+[35] coreutils ''')
+slowprint('''\033[96m
+This Command for access Storage in Termux
+[00] termux-setup-storage''')
+print ("                                            ")
+choice = input("\033[93mDo You Want to Install All Packages [y/n] : ")
+if choice == 'n' : sys.exit()
+if choice == 'y' : os.system ("apt update")
+os.system ("atp upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("apt install git -y")
+os.system ("apt install termux-setup-storage -y")
+os.system ("apt install chmod +x * -y")
+os.system ("apt install toilet -y")
+os.system ("apt install unzip -y")
+os.system ("apt install termcolor -y")
+os.system ("apt install sudo -y")
+os.system ("apt install ngrok -y")
+os.system ("apt install screenfetch -y")
+os.system ("apt install cmatrix -y")
+os.system ("apt install tor -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash")
+ 
+print ("wait for second and start hacking")
+ 
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+ 
+ 
+print ("""
+MAFIA-KILLER 💕GRAY HAT HACKER💕 """)
+ 
+os.system ("apt install ruby -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+ 
+print ("Allow the Button For Access the Storage in Termux")
+ 
+ 
+os.system ("termux-setup-storage")
+ 
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+print("\033[92m+-------------------------------------------------+")
+slowprint('''\033[95m|             Welcome To Hackers World            |
+|           whatsapp no 03132197796         |
+| MAFIA-KILLER ( GRAY HAT HACKER )  |''')
+print("+-------------------------------------------------+")
+ 
+input("\n\nPress the enter key to exit : ")
